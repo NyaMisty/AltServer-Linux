8284aaedee7625beb856cee4a14a47d049295cc6
Misty <gyc990326@gmail.com>
2022-04-16 20:11:38 +0800

    [libraries] Bumping to latest libimobiledevice

--- makefiles/libimobiledevice-build/config.h
+++ makefiles/libimobiledevice-build/config.h
@@ -1,45 +1,195 @@
+// config for Alpine 3.15
+
+/* Define to 1 if you have the `asprintf' function. */
 #define HAVE_ASPRINTF 1
+
+/* Define if the C compiler supports constructor/destructor attributes */
+#define HAVE_ATTRIBUTE_CONSTRUCTOR 1
+
+/* define if struct dirent has member d_type */
 #define HAVE_DIRENT_D_TYPE 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
+
+/* Define if compiled with -fvisibility=hidden */
 #define HAVE_FVISIBILITY 1
-#define HAVE_GCRYPT_H 1
+
+/* Define if you have libgcrypt support */
+/* #undef HAVE_GCRYPT */
+
+/* Define to 1 if you have the <gcrypt.h> header file. */
+/* #undef HAVE_GCRYPT_H */
+
+/* Define to 1 if you have the `getifaddrs' function. */
 #define HAVE_GETIFADDRS 1
-#define HAVE_GMTIME_R 1
+
+/* Define if you have GnuTLS support */
+/* #undef HAVE_GNUTLS */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
-#define HAVE_LOCALTIME_R 1
-#define HAVE_MALLOC 1
-#define HAVE_MEMMEM 1
-#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Define if you have MbedTLS support */
+/* #undef HAVE_MBEDTLS */
+
+/* Define if you have OpenSSL support */
 #define HAVE_OPENSSL 1
-#define HAVE_PTHREAD 1
-#define HAVE_PTHREAD_CANCEL 1
-#define HAVE_PTHREAD_PRIO_INHERIT 1
-#define HAVE_REALLOC 1
-#define HAVE_SLEEP 1
+
+/* Define if libplist has JSON support */
+#define HAVE_PLIST_JSON 1
+
+/* If available, contains the Python version number currently in use. */
+/* #undef HAVE_PYTHON */
+
+/* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `stpcpy' function. */
 #define HAVE_STPCPY 1
+
+/* Define to 1 if you have the `stpncpy' function. */
 #define HAVE_STPNCPY 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
 #define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the `strdup' function. */
 #define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
 #define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strndup' function. */
 #define HAVE_STRNDUP 1
-#define HAVE_STRPTIME 1
+
+
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
-#define HAVE_TIMEGM 1
-#define HAVE_TM_TM_GMTOFF 1
-#define HAVE_TM_TM_ZONE 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vasprintf' function. */
 #define HAVE_VASPRINTF 1
-#define PACKAGE_STRING "jitterbug 1.0"
-#define __BIG_ENDIAN 4321
-#define __LITTLE_ENDIAN 1234
 
-#ifdef __BYTE_ORDER
-#undef __BYTE_ORDER
-#endif
-#define __BYTE_ORDER 1234
+/* Define if building with wireless pairing support */
+#undef HAVE_WIRELESS_PAIRING
 
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Define if debug message output code should not be built. */
+#undef STRIP_DEBUG_CODE
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Define to 1 if you have the `pthread_cancel' function. */
+#define HAVE_PTHREAD_CANCEL 1
+
+/* Define to 1 if you have the `pthread_once' function. */
+#define HAVE_PTHREAD_ONCE 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+
+/* Define to 1 if you have the `gmtime_r' function. */
+#define HAVE_GMTIME_R 1
+
+/* Define to 1 if you have the `localtime_r' function. */
+#define HAVE_LOCALTIME_R 1
+
+/* Define to 1 if you have the `memmem' function. */
+#define HAVE_MEMMEM 1
+
+/* If available, contains the Python version number currently in use. */
+/* #undef HAVE_PYTHON */
+
+/* Define to 1 if you have the `strptime' function. */
+#define HAVE_STRPTIME 1
+
+/* Define to 1 if you have the `timegm' function. */
+#define HAVE_TIMEGM 1
+
+/* Define if struct tm has a tm_gmtoff member */
+#define HAVE_TM_TM_GMTOFF 1
+
+/* Define if struct tm has a tm_zone member */
+#define HAVE_TM_TM_ZONE 1
+
+/* little endian */
+#define __LITTLE_ENDIAN__ 1
+
+/* Define if you have inotify support (linux only) */
+#define HAVE_INOTIFY 1
+
+
+/* Define if you have program_invocation_short_name */
+#define HAVE_PROGRAM_INVOCATION_SHORT_NAME 1
+
+/* Define if program_invocation_short_name is declared in errno.h */
+/* #undef HAVE_PROGRAM_INVOCATION_SHORT_NAME_ERRNO_H */
+
+/* Define to 1 if you have the `pselect' function. */
+#define HAVE_PSELECT 1
+
+/* Define to 1 if you have the `realloc' function. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if you have the `sleep' function. */
+#define HAVE_SLEEP 1
+
+/* Define to 1 if you have the <sys/inotify.h> header file. */
+#define HAVE_SYS_INOTIFY_H 1
+
+/* Define if lstat syscall is supported */
+#define HAVE_LSTAT 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+
+
+/* Name of package */
+//#define PACKAGE "AltServer-Linux"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "AltServer-Linux"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "AltServer-Linux "
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "AltServer-Linux"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/NyaMisty/AltServer-Linux"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
--- makefiles/libimobiledevice-build/libimobiledevice-files.mak
+++ makefiles/libimobiledevice-build/libimobiledevice-files.mak
@@ -1,7 +1,8 @@
-libimobiledevice_src := $(wildcard $(LIB_DIR)/libimobiledevice/src/*.c) $(wildcard $(LIB_DIR)/libimobiledevice/common/*.c)
+libimobiledevice_src += $(wildcard $(LIB_DIR)/libimobiledevice/src/*.c) $(wildcard $(LIB_DIR)/libimobiledevice/common/*.c)
+libimobiledevice_src += $(wildcard $(LIB_DIR)/libimobiledevice-glue/src/*.c)
 libimobiledevice_src += $(wildcard $(LIB_DIR)/libusbmuxd/src/*.c)
 libimobiledevice_src += $(wildcard $(LIB_DIR)/libusbmuxd/common/*.c)
-libimobiledevice_include := -I$(LIB_DIR)/libimobiledevice/include -I$(LIB_DIR)/libimobiledevice -I$(LIB_DIR)/libusbmuxd/include
+libimobiledevice_include := -I$(LIB_DIR)/libimobiledevice/include -I$(LIB_DIR)/libimobiledevice-glue/include -I$(LIB_DIR)/libimobiledevice -I$(LIB_DIR)/libusbmuxd/include
 
 libplist_include := -I$(LIB_DIR)/libplist/include
 libplist_src := $(wildcard $(LIB_DIR)/libplist/src/*.c) $(LIB_DIR)/libplist/libcnary/node.c $(LIB_DIR)/libplist/libcnary/node_list.c
