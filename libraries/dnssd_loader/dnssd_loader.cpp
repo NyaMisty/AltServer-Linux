25b2b7f66f27d6b20aef4caf47f44fbacb6d4380
Misty <gyc990326@gmail.com>
2022-04-16 14:33:31 +0800

    [server] add mDNS support

--- libraries/dnssd_loader/dnssd_loader.cpp
+++ libraries/dnssd_loader/dnssd_loader.cpp
@@ -1,4 +1,10 @@
 #include "dns_sd.h"
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/prctl.h> // prctl(), PR_SET_PDEATHSIG
+#include <signal.h> // signals
+
 
 DNSServiceErrorType DNSSD_API DNSServiceRegister
     (
@@ -15,8 +21,53 @@ DNSServiceErrorType DNSSD_API DNSServiceRegister
     DNSServiceRegisterReply             callBack,      /* may be NULL */
     void                                *context       /* may be NULL */
     ) {
+        // python3 -c 'from ctypes import *; sdRef = c_int(); CDLL('libdns_sd.so').DNSServiceRegister(byref(sdRef), flags, interfaceIndex, name, regtype, domain, host, txtLen, txtRecord, None, None)'
+        std::string pyCommand = "from ctypes import *; dll = CDLL('libdns_sd.so'); ";
 
-        // XXX TODO: Wait for usbmuxd linux to support network device
+        pyCommand += "sdRef = c_int(); ";
+#define INT_ARG(argname) (std::string("") + #argname " = " + std::to_string(argname) + "; ")
+#define STR_ARG(argname) (argname ? std::string(#argname " = br'") + argname + "'; " : std::string(#argname " = None; "))
+        pyCommand += INT_ARG(flags);
+        pyCommand += INT_ARG(interfaceIndex);
+        pyCommand += STR_ARG(name);
+        pyCommand += STR_ARG(regtype);
+        pyCommand += STR_ARG(domain);
+        pyCommand += STR_ARG(host);
+        pyCommand += INT_ARG(port);
+        pyCommand += INT_ARG(txtLen);
+        
+        std::string txtRecordHex = "";
+        for (int i = 0; i < txtLen; i++) {
+            char buf[16] = { 0 };
+            sprintf(buf, "\\x%02X", *((char *)txtRecord + i));
+            txtRecordHex += buf;
+        }
+        pyCommand += "txtRecord = b'" + txtRecordHex + "'; ";
+        pyCommand += "ret = dll.DNSServiceRegister(byref(sdRef), flags, interfaceIndex, name, regtype, domain, host, port, txtLen, txtRecord, None, None); ";
+        pyCommand += "print('DNSServiceRegister result: %d' % ret); ";
+
+        pyCommand += "from threading import Event; Event().wait(); ";
+        
+        printf("Running python3 command to advertise AltServer: %s\n", pyCommand.c_str());
+
+        pid_t ppid_before_fork = getpid();
+        int child,status;
+        if ((child = fork()) < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+        if(child == 0){
+            int r = prctl(PR_SET_PDEATHSIG, SIGTERM);
+            if (r == -1) { perror(0); exit(1); }
+            // test in case the original parent exited just
+            // before the prctl() call
+            if (getppid() != ppid_before_fork)
+                exit(1);
+            execlp("python3", "python3", "-c", pyCommand.c_str(), NULL);
+            exit(1);
+        } else {
+            ;
+        }
         return 0;
     }
 
